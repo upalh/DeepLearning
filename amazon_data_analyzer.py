33b04e18f92e18b35947dace0073bbdbdbfad278
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-01-22 08:01:34 +0100

    commit of parser and initial analysis on data

---
+++ amazon_data_analyzer.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Class to perform some analysis on the amazon dataset using mrjob.
+
+Created on Sun Jan 19 05:46:37 2014
+
+@author: mhasan
+
+"""
+import sys
+import json
+from mrjob.job import MRJob
+
+class AmazonDataAnalyzer(MRJob):
+
+    def configure_options(self):
+        super(AmazonDataAnalyzer, self).configure_options()
+        self.add_passthrough_option(
+            '--customer', help="customer key")
+        self.add_passthrough_option(
+            '--customer_index', help="customer index within customer array.")                        
+        self.add_passthrough_option(
+            '--product', help="product key (ASIN).")
+        self.add_passthrough_option(
+            '--similar', help="similar product key.")            
+        self.add_passthrough_option(
+            '--metric', help="metric for evaluation: '1' for products that " \
+                                "have no customers. '2' for products that " \
+                                "are discontinued. '3' for number of customers " \
+                                "that have bought just 1 product. '4' for " \
+                                "distribution of # product purchases.")                        
+    
+        
+    def mapper(self, _, line):
+        productKey = self.options.product
+        customerKey = self.options.customer
+        customerIdx = int(self.options.customer_index)        
+        similarKey = self.options.similar
+        metric = int(self.options.metric)
+        
+        parsedLine = json.loads(line)        
+        
+        if metric == 1:
+            # how many products have no customers ==> "no_customers"	139960
+            if customerKey in parsedLine and not parsedLine[customerKey]:
+                yield "no_customers", 1
+
+        elif metric == 2:
+            # how many discontinued products ==> "discontinued_products" 5868
+            if customerKey not in parsedLine and similarKey not in parsedLine:
+                yield "discontinued_products", 1
+
+        elif metric == 3:
+            # how many customers bought just one product
+            # in the reducer we will have to filter all the customer IDs
+            # whose count is greater than 1.        
+            # there appear to be 1555170 customer entries, but 744954 have only
+            #   bought 1 product
+            customerList = parsedLine[customerKey] if customerKey in parsedLine else None
+            if customerList:
+                for customer in customerList:
+                    yield "customer_" + customer[customerIdx], 1
+        
+        else:            
+            # distribution of # product purchases
+            # there are 721342 unique ASINS, but a total of 958609 ASINS, 
+            # and 548552 records, so only about 200k of the actual records were
+            # in the similar sections. the others were new that don't have a 
+            # corresponding record for it.
+            if productKey in parsedLine:        
+                yield "product_" + parsedLine[productKey], 1
+                
+            similarList = parsedLine[similarKey] if similarKey in parsedLine else None                
+            if similarList:
+                for similarProduct in similarList:
+                    yield "product_" + similarProduct, 1
+            
+        
+    def reducer(self, key, values):
+        if "customer_" in key:
+            valueSums = sum(values)
+            if valueSums == 1:
+                yield key, valueSums
+        else:
+            yield key, sum(values)
+
+
+if __name__ == '__main__':
+    AmazonDataAnalyzer.run()
+    
+
---
+++ plot_data.py
@@ -0,0 +1,66 @@
+"""
+Created on Mon Jan 20 07:50:52 2014
+
+@author: ubuntu
+
+"""
+
+import sys
+import pylab as pl
+
+# this will dictate how many bars we've got. based on the data,
+# the number of purchases for any given product ranges from 0 - 99, so
+# i just have a 100 buckets. each bucket will store a count of the number
+# of products that have been bought X times, where X varies from 0 - 99.
+NUM_BUCKETS = 100
+
+if __name__ == "__main__":
+    
+    if(len(sys.argv)>1):
+        path_to_file = sys.argv[1]
+    else:
+        path_to_file = "numPurchaseDistributions"
+
+    # create our label vectors
+    labels = []
+    for label in range(0, NUM_BUCKETS):
+        labels.append(label)
+        
+    # now calculate the number of products for each of our buckets.
+    bucketCounts = dict()
+    for line in open(path_to_file):
+        x, y = line.split()
+
+        def insert(bucket):
+            if str(bucket) not in bucketCounts:
+                bucketCounts[str(bucket)] = 1 
+            else:
+                bucketCounts[str(bucket)] = bucketCounts[str(bucket)] + 1
+            
+        if int(y) > NUM_BUCKETS:
+            insert(NUM_BUCKETS)
+        else:            
+            bucket = int(y) % NUM_BUCKETS
+            insert(bucket)
+
+    # get the counts in a proper order in the list to plot
+    values = []
+    for label in range(0, NUM_BUCKETS):
+        value = bucketCounts[str(label)] if str(label) in bucketCounts else 0
+        values.append(value)
+        
+    fig = pl.figure()
+    ax = pl.subplot(111)
+    ax.bar(labels, values)
+    
+    #Create a y label
+    ax.set_ylabel('Number of products')
+     
+    # Create a title, in italics
+    ax.set_title('Number of purchases',fontstyle='italic')
+     
+    # This sets the ticks on the x axis to be exactly where we put
+    # the center of the bars.
+    ax.set_xticks(range(NUM_BUCKETS))
+    
+    pl.show()
